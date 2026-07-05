44e705fa7620a2cd4f33052f772d1a47e452320c
Mia-learn-to-code <mialasifa@gmail.com>
2026-07-05 09:20:53 +0000

    Save try2

---
+++ try1.js
@@ -0,0 +1,5 @@
+let position = 1
+
+for (let step = 1; step <= 5; step++) {
+        console.log("Move step " + step);
+}
---
+++ try2.js
@@ -0,0 +1,7 @@
+for (let step = 1; step <= 12; step++){
+    console.log("Move forward " + step);
+    console.log("Turn right 30 degree")
+}
+console.log ("=============");
+console.log("Robot turn around")
+    
